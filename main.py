bb0e7c20dadf124bfa3f4faff3099e5af647c04a
justtaara <64958569+justtaara@users.noreply.github.com>
2021-06-07 03:48:26 +0200

    Update main.py
    Przerobienie bohatera na element klasy Player, żeby dalsza część pracy lepiej ze sobą współgrała
    (nie wygląda, ale było to wyzwanie....)

--- main.py
+++ main.py
@@ -22,25 +22,63 @@ def przycisk_koniec():
     pygame.quit()
 def przycisk_ponownie():
     pass #jeszcze nie wiem, jak to zrobić
-#Grafika - tu wrzucajcie ikonki itp
-playerPng = pygame.image.load('lion.png')
 
 #########CZAS##########
 timer_stop = datetime.datetime.utcnow() +datetime.timedelta(seconds=90) #SEKUNDY ODLICZANIE - 1,5 minuty
 
-#######GRACZ####### - na razie bez klasy
-player_X = int(300)
-player_Y = int(300)
-player_HP = 90
-#zmienne początkowe do ruchu
-player_mov_X = int(0)
-player_mov_Y = int(0)
-#gracz - położenie
-#rysowanie gracza
-def player_appear():
-    screen.blit(playerPng,(int(player_X), int(player_Y)))
-    player_box = pygame.Rect(int(player_X), int(player_Y), 35, 40)
-    return player_box
+#######GRACZ#######
+class Player:
+    def __init__(self):
+        self.player_X = 300
+        self.player_Y = 300
+        self.mov_X = 0
+        self.mov_Y = 0
+
+    def player_appear(self):
+        playerPng = pygame.image.load('lion.png')
+        screen.blit(playerPng,(int(self.player_X), int(self.player_Y)))
+        player_box = pygame.Rect(int(self.player_X), int(self.player_Y),int(self.player_X), int(self.player_Y))
+
+    def player_move(self):
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                print('KEYDOWN')
+#działanie strzałek lub awsd - przyciskanie: (ruch gracza)
+                if event.key == pygame.K_LEFT or event.key == ord('a'):
+                    self.mov_X = -4
+                if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                    self.mov_X = 4
+                if event.key == pygame.K_UP or event.key == ord('w'):
+                    self.mov_Y = -4
+                if event.key == pygame.K_DOWN or event.key == ord('s'):
+                    self.mov_Y = 4
+        #działanie strzałek lub awsd - odciśnięcie: (ruch gracza stop)
+            if event.type == KEYUP:
+                print('KEYUP')
+                if event.key == pygame.K_LEFT or event.key == ord('a'):
+                    self.mov_X = 0
+                if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                    self.mov_X = 0
+                if event.key == pygame.K_UP or event.key == ord('w'):
+                    self.mov_Y = 0
+                if event.key == pygame.K_DOWN or event.key == ord('s'):
+                    self.mov_Y = 0
+        #gracz nie wchodzi w ściany
+        if self.player_Y <= 0:
+            self.player_Y = 0
+        if self.player_Y >=575:
+            self.player_Y = 575
+        if self.player_X <= 0:
+            self.player_X = 0
+        if self.player_X >= 575:
+            self.player_X = 575
+        #aktualizacja ruchu gracza - początkowa pozycja + zmiana
+        #print(self.mov_X, self.mov_Y)
+        self.player_X += self.mov_X
+        self.player_Y += self.mov_Y
+        #print(self.player_X, self.player_Y)
+p = Player()
+
 ##SPRAWDZANIE, CZY NASTĄPIŁA DETEKCJA OBIEKTU###
 def is_detected(width1, height1, width2, height2):
     if width1 >= width2 and width1 <= width2 + 32: #32x32 wymiar ikonki
@@ -53,7 +91,7 @@ class Herbivore(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('monkey.png')
-        self.rect = self.image.get_rect() 
+        self.rect = self.image.get_rect()
         self.rect.x = random.randint(0,575)
         self.rect.y = random.randint(0,575)
         self.HP = 90 #startowy poziom
@@ -89,14 +127,14 @@ fruits = []
 number_of_fruits = randint(15,25)
 for _ in range(number_of_fruits):
     fruits.append(Fruit())
-    
+
 NUM_CARNIVORE = 4
 carnivores = []
 for i in range(NUM_CARNIVORE):
     carnivores.append(Carnivore())
-    
+
 our_sprites = pygame.sprite.Group() #sprite obsługuje wszystkie poruszające się obiekty w grze #do our_sprites wrzucamy wszystkie poruszające się elementy w grze
-herbivores = pygame.sprite.Group() #klasy przedmiotów tworzą osobne grupy 
+herbivores = pygame.sprite.Group() #klasy przedmiotów tworzą osobne grupy
 for _ in range(10):
     herbivore = Herbivore()
     our_sprites.add(herbivore)
@@ -124,52 +162,22 @@ while running:
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE: #wyjście z gry przyciskiem escape
                 running = False
-#działanie strzałek lub awsd - przyciskanie: (ruch gracza)
-            if event.key == pygame.K_LEFT or event.key == ord('a'):
-                player_mov_X = -1.2
-            if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                player_mov_X = 1.2
-            if event.key == pygame.K_UP or event.key == ord('w'):
-                player_mov_Y = -1.2
-            if event.key == pygame.K_DOWN or event.key == ord('s'):
-                player_mov_Y = 1.2
-#działanie strzałek lub awsd - odciśnięcie: (ruch gracza stop)
-        if event.type == KEYUP:
-            if event.key == pygame.K_LEFT or event.key == ord('a'):
-                player_mov_X = 0
-            if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                player_mov_X = 0
-            if event.key == pygame.K_UP or event.key == ord('w'):
-                player_mov_Y = 0
-            if event.key == pygame.K_DOWN or event.key == ord('s'):
-                player_mov_Y = 0
-#gracz nie wchodzi w ściany
-    if player_Y <= 0:
-        player_Y = 0
-    if player_Y >=575:
-        player_Y = 575
-    if player_X <= 0:
-        player_X = 0
-    if player_X >= 575:
-        player_X = 575
+
 #zielone tło
     screen.fill((127, 255, 0))
     our_sprites.update() #aktualnie zawiera jedynie chodzenie roślinożerców
-    time.sleep(0.2) #opóźnia update, dzięki czemu roślinożercy nie są rozedrgani 
-#aktualizacja ruchu gracza - początkowa pozycja + zmiana
-    player_X += player_mov_X
-    player_Y += player_mov_Y
+    time.sleep(0.2) #opóźnia update, dzięki czemu roślinożercy nie są rozedrgani
 
     for fruit in fruits:
         fruit.appear()
-    our_sprites.draw(screen) #to co wrzuciłyśmy do Sprite pojawia się na ekranie 
-    for fruit in fruits[:]: 
+    our_sprites.draw(screen) #to co wrzuciłyśmy do Sprite pojawia się na ekranie
+    for fruit in fruits[:]:
         if is_detected(herbivore.x, herbivore.y, fruit.x, fruit.y) == True:
             fruits.remove(fruit)
 
-#pojawienie się gracza na planszy
-    player_appear()
-
+#gracz się pojawia
+    p.player_appear()
+    p.player_move()
 
     for carnivore in carnivores:
         carnivore.move()
@@ -178,3 +186,4 @@ while running:
 #koniec pętli
     pygame.display.flip()
 pygame.quit()
+
